aadb138f3aa6fc51618ddddc6ea386480712e233
ostnam <tsougaevm@gmail.com>
2023-02-18 14:27:33 +0100

    example 1

--- examples/Example.hs
+++ examples/Example.hs
@@ -2,21 +2,54 @@
 module Main (main) where
 
 import Rssify
-import Text.RSS.Syntax
-import Text.Feed.Types
+import Text.Feed.Types ( Feed (RSS1Feed) )
+import qualified Text.RSS1.Syntax as RSS ( Item (..), Feed (..), nullChannel )
+import qualified Data.Text as T
+import qualified Text.HTML.TagSoup as TagSoup
 
 example :: RssifyApp
-example = FromHtml "https://www.google.com" fn settings
-  where fn _ = RSSFeed $ RSS "1.0" [] (nullChannel "example" "/example") [] 
-        settings = RssifyAppSettings 10 "/example"
+example = FromHtml "https://news.ycombinator.com/news" parseHn settings
+  where settings = RssifyAppSettings { refreshInterval = 10
+                                     , url = "/example"
+                                     }
 
-example2 :: RssifyApp
-example2 = FromHtml "https://www.google.com" fn settings
-  where fn _ = RSSFeed $ RSS "1.0" [] (nullChannel "example 2" "/example") [] 
-        settings = RssifyAppSettings 10 "/example2"
+parseHn :: [TagSoup.Tag T.Text] -> Feed
+parseHn tagList = buildHNFeed (uncurry toHNEntry <$> getTitles tagList)
+  where getTitles :: [TagSoup.Tag T.Text] -> [(T.Text, T.Text)]
+        getTitles ( TagSoup.TagOpen "span" [("class", "titleline")]
+                  : TagSoup.TagOpen "a" [("href", link)]
+                  : TagSoup.TagText title
+                  : rest) = (title, link) : getTitles rest
+        getTitles (_:xs) = getTitles xs
+        getTitles [] = []
+
+        toHNEntry :: T.Text -> T.Text -> RSS.Item
+        toHNEntry title link = RSS.Item
+          { RSS.itemURI = link
+          , RSS.itemTitle = title
+          , RSS.itemLink = link
+          , RSS.itemDesc = Nothing
+          , RSS.itemDC = []
+          , RSS.itemTopics = []
+          , RSS.itemContent = []
+          , RSS.itemOther = []
+          , RSS.itemAttrs = []
+          }
+
+        buildHNFeed :: [RSS.Item] -> Feed
+        buildHNFeed items = RSS1Feed $ RSS.Feed
+          { RSS.feedVersion = "1"
+          , RSS.feedChannel = RSS.nullChannel "" "hn"
+          , RSS.feedImage = Nothing
+          , RSS.feedItems = items
+          , RSS.feedTextInput = Nothing
+          , RSS.feedTopics = []
+          , RSS.feedOther = []
+          , RSS.feedAttrs = []
+          }
+
 
 main :: IO ()
 main = rssify [ example
-              , example2
               ]
-                
+
